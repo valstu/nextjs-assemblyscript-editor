ac7bdd278b2242453ef0be13a362aa8bea52068f
Valtteri Karesto <valtteri.karesto@gmail.com>
2022-04-12 14:44:52 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    config.experiments = {
+      topLevelAwait: true,
+      layers: true,
+    };
+    config.resolve.fallback = {
+      fs: false,
+      process: false,
+      buffer: false,
+      module: false,
+      path: false,
+    };
+    return config;
+  },
+  // experimental: { esmExternals: true },
+  // experimental: {
+  //   paths: {
+  //     binaryen: ["./node_modules/binaryen/index.js"],
+  //     long: ["./node_modules/long/index.js"],
+  //     assemblyscript: ["./node_modules/assemblyscript/index.js"],
+  //     "assemblyscript/asc": ["./node_modules/assemblyscript/dist/asc.js"],
+  //   },
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
